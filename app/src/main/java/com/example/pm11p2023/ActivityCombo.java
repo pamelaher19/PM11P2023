5af96893eb45e7ba54c2098d9e9d2030d2d5150f
pamelaher19 <katherinh692@gmail.com>
2023-02-16 13:02:54 -0600

    comentarios sobre activity combo

--- app/src/main/java/com/example/pm11p2023/ActivityCombo.java
+++ app/src/main/java/com/example/pm11p2023/ActivityCombo.java
@@ -10,5 +10,7 @@ public class ActivityCombo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_combo);
+
+        // Hola esto es un cambio //
     }
 }
